d4f2dfe55dda77ce37a531f7c954387bc8f7d632
Vũ Nguyễn <longvuxautrai12@gmail.com>
2024-05-26 22:51:11 +0700

    update

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -25,7 +25,7 @@ import Link from "next/link";
 const { TextArea } = Input;
 
 const Chat = () => {
-  const { lastJsonMessage }: any = useWebSocket("wss://cho-tot-be.onrender.com:8085");
+  const { lastJsonMessage }: any = useWebSocket("wss://cho-tot-be.onrender.com");
   const dispatch = useDispatch<AppDispatch>();
   const router = useRouter();
   const { allConversation, conversation, allConversationSummary } = useSelector((state: RootState) => state.chat);
